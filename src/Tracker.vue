c9ff484a5a8c4551a53ec828bf4add724fe2df83
Mark Hayes <me@markhay.es>
2017-01-29 18:33:25 -0800

    use sass instead of scss lang
    even though webpack-simple is supposed to support this syntax, there are warning about scss loader not being found

--- src/Tracker.vue
+++ src/Tracker.vue
@@ -33,7 +33,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="sass">
 @import '~bulma/sass/utilities/variables';
 @import "~progress-tracker/app/styles/_progress-tracker.scss";
 
